36f07a81486002ff469f5fd515aad293c81e837c
pvr <pvr@pvrs-MacBook-Pro.local>
2019-07-02 17:04:33 +0200

    Added first version of function to construct comparison plot report

--- R/compose_compare_plot.R
+++ R/compose_compare_plot.R
@@ -17,27 +17,158 @@
 #'
 #' @param ps_gedir  directory with plots of current GE round
 #' @param ps_archdir  archive directory with plots from previous GE
+#' @param pstrgdir    target directory where plot files from archive are extracted, relative to ps_gedir
 #' @param ps_templ  path to Rmarkdown template file
+#' @param ps_report_text text that is included in the report before plotting
+#' @param ps_rmd_report name of report source file
+#' @param pb_debug flag indicating whether debug info is printed
 #'
 create_ge_plot_report <- function(ps_gedir,
                                   ps_archdir,
+                                  ps_trgdir,
                                   ps_templ,
-                                  ps_rmd_report = 'ge_plot_report.Rmd'){
+                                  ps_report_text,
+                                  ps_rmd_report = 'ge_plot_report.Rmd',
+                                  pb_debug      = FALSE){
+
+  if (pb_debug) cat(" * Starting create_ge_plot_report ... \n")
+
+  # ps_gedir and ps_archdir must esits
+  if (!dir.exists(ps_gedir))
+    stop("[ERROR -- create_ge_plot_report] Cannot find plot directory: ", ps_gedir, "\n")
+
+  if (!dir.exists(ps_archdir))
+    stop("[ERROR -- create_ge_plot_report] Cannot find archive directory: ", ps_archdir, "\n")
 
   # ps_templ must exist
   if (!file.exists(ps_templ))
-    stop("[ERROR -- create_ge_plot_report] Cannot find Rmd template: ", ps_templ)
+    stop("[ERROR -- create_ge_plot_report] Cannot find Rmd template: ", ps_templ, "\n")
+
+  # target directory should be relative to ps_gedir
+  if (fs::is_absolute_path(path = ps_trgdir))
+    stop("[ERROR -- create_ge_plot_report] Target directory: ", ps_trgdir, " must be relative to ps_gedir: ", ps_gedir, "\n")
+  # append ps_trgdir to ps_gedir
+  s_trgdir <- file.path(ps_gedir, ps_trgdir)
+  if (pb_debug) cat(" * Absolute target directory: ", s_trgdir, "\n")
+
+  # get the root subdirectory in ps_gedir where ps_trgdir is added
+  s_trgroot <- file.path(ps_gedir, fs::path_split(ps_trgdir)[[1]][1])
+  if (pb_debug) cat(" * Target root directory: ", s_trgroot, "\n")
+
+  # if the ps_trgdir does not exist, create it
+  if (!dir.exists(s_trgdir)) {
+    if (pb_debug) cat(" * Create target directory: ",s_trgdir, "\n")
+    dir.create(path = s_trgdir, recursive = TRUE)
+  }
+
+  # if target directory could not be created, stop here
+  if (!dir.exists(s_trgdir))
+    stop("[ERROR -- create_ge_plot_report] Cannot create target directory: ", s_trgdir, "\n")
 
   # rename the template into the result report
-  file.rename(from = ps_templ, to = ps_rmd_report)
+  file.copy(from = ps_templ, to = ps_rmd_report)
 
-  # loop over the plot files in ps_gedir
+  # add the report text to the report
+  cat(ps_report_text, "\n\n", file = ps_rmd_report, append = TRUE)
+
+  # extract plot files from current ps_gedir
   vec_plot_files_ge <- list.files(ps_gedir, pattern = "\\.png$|\\.pdf$", full.names = TRUE)
+  if (pb_debug){
+    cat("Plot files in ", ps_gedir, "\n")
+    print(vec_plot_files_ge)
+  }
+  # loop over plot files and get corresponding plot file from previous ge, if it exists
+  for (f in vec_plot_files_ge){
+    if (pb_debug) cat(" * Plot file: ", f, "\n")
+    # write the chunk start into the report file
+    cat("\n```{r, echo=FALSE, fig.show='hold', out.width='50%'}\n", file = ps_rmd_report, append = TRUE)
+    # check whether corresponding plot file existed in archive
+    bnf <- basename(f)
+    bnfgz <- paste(bnf, "gz", sep = ".")
+    bnfgzpath <- file.path(ps_archdir, bnfgz)
+    if (file.exists(bnfgzpath)){
+      if (pb_debug) cat(" * Found archived plot file: ", bnfgz, " in ", ps_archdir, "\n")
+      # copy file from archive
+      if (pb_debug) cat(" * Copy from: ", bnfgzpath, " to ", s_trgdir, "\n")
+      file.copy(from = bnfgzpath, to = s_trgdir)
+      bnfgztrgpath <- file.path(s_trgdir, bnfgz)
+      bnftrgpath <- file.path(s_trgdir, bnf)
+      if (!file.exists(bnftrgpath)){
+        if (pb_debug) cat(" * Unzip: ", bnfgztrgpath, "\n")
+        R.utils::gunzip(bnfgztrgpath)
+      } else {
+        if (pb_debug) cat(" * File: ", bnftrgpath, " already exists", "\n")
+      }
+      ### # Include extracted file into the report
+      cat(paste0("knitr::include_graphics(path = '", bnftrgpath, "')\n", collapse = ""), file = ps_rmd_report, append = TRUE)
+    } else {
+      if (pb_debug) cat(" * Cannot find archived plot file: ", bnfgz, " in ", ps_archdir, "\n")
+    }
+    # include current plot file into report
+    cat(paste0("knitr::include_graphics(path = '", f, "')\n", collapse = ""), file = ps_rmd_report, append = TRUE)
+    # write junk end into report
+    cat("```\n\n", file = ps_rmd_report, append = TRUE)
+  }
+  # finally include session info into the report
+  cat("\n```{r}\n sessioninfo::session_info()\n```\n\n", file = ps_rmd_report, append = TRUE)
+  # render the generated Rmd file
+  rmarkdown::render(input = ps_rmd_report)
+  # move pdf of report to ps_ge_dir
+  s_pdf_report <- fs::path_ext_set(fs::path_ext_remove(ps_rmd_report), "pdf")
+  if (pb_debug) cat(" * Moving report ", s_pdf_report, " to: ", ps_gedir, "\n")
+  fs::file_move(s_pdf_report, ps_gedir)
+  # remove report source
+  if (pb_debug) cat(" * Removing report rmd source ", ps_rmd_report, " to: ", ps_gedir, "\n")
+  fs::file_delete(ps_rmd_report)
+
+  # remove target root dir
+  if (pb_debug) cat(" * Delete target root directory: ", s_trgroot, "\n")
+  fs::dir_delete(path = s_trgroot)
+
+  if (pb_debug) cat(" * End of create_ge_plot_report\n")
+
+  # return nothing
+  return(invisible(NULL))
 }
 
-### # Testing stuff
+### # Testing stuff
+
+# local test call
+# create_ge_plot_report(ps_gedir = "inst/extdata/compare-plot",
+#                       ps_archdir = "inst/extdata/archive/compare-plot",
+#                       ps_templ   = "inst/templates/compare_plots.Rmd.template")
+#
+# create_ge_plot_report(ps_gedir       = "/Volumes/data_zws/fbk/work/bv/YearMinus0/compareBull",
+#                       ps_archdir     = "/Volumes/data_archiv/zws/1904/fbk/work/bv/YearMinus0/compareBull",
+#                       ps_trgdir      = "1904/compareBull",
+#                       ps_templ       = "inst/templates/compare_plots.Rmd.template",
+#                       ps_report_text = "## Comparison Of Plots\nPlots compare estimates of fbk for bulls of breed BV between GE-1904 on the left and the current GE-1908 on the right.",
+#                       ps_rmd_report  = 'ge_plot_report_fbk_compareBulls_bv.Rmd',
+#                       pb_debug       = TRUE)
+#
+# create_ge_plot_report(ps_gedir       = "/Volumes/data_zws/fbk/work/bv/YearMinus0/compareCow",
+#                       ps_archdir     = "/Volumes/data_archiv/zws/1904/fbk/work/bv/YearMinus0/compareCow",
+#                       ps_trgdir      = "1904/compareCow",
+#                       ps_templ       = "inst/templates/compare_plots.Rmd.template",
+#                       ps_report_text = "## Comparison Of Plots\nPlots compare estimates of fbk for cows of breed BV between GE-1904 on the left and the current GE-1908 on the right.",
+#                       ps_rmd_report  = 'ge_plot_report_fbk_compareCow_bv.Rmd',
+#                       pb_debug       = TRUE)
+
+create_ge_plot_report(ps_gedir       = "/Volumes/data_zws/fbk/work/rh/YearMinus0/compareBull",
+                      ps_archdir     = "/Volumes/data_archiv/zws/1904/fbk/work/rh/YearMinus0/compareBull",
+                      ps_trgdir      = "1904/compareBull",
+                      ps_templ       = "inst/templates/compare_plots.Rmd.template",
+                      ps_report_text = "## Comparison Of Plots\nPlots compare estimates of fbk for bulls of breed RH between GE-1904 on the left and the current GE-1908 on the right.",
+                      ps_rmd_report  = 'ge_plot_report_fbk_compareBulls_rh.Rmd',
+                      pb_debug       = TRUE)
+
+create_ge_plot_report(ps_gedir       = "/Volumes/data_zws/fbk/work/rh/YearMinus0/compareCow",
+                      ps_archdir     = "/Volumes/data_archiv/zws/1904/fbk/work/rh/YearMinus0/compareCow",
+                      ps_trgdir      = "1904/compareCow",
+                      ps_templ       = "inst/templates/compare_plots.Rmd.template",
+                      ps_report_text = "## Comparison Of Plots\nPlots compare estimates of fbk for cows of breed RH between GE-1904 on the left and the current GE-1908 on the right.",
+                      ps_rmd_report  = 'ge_plot_report_fbk_compareCow_rh.Rmd',
+                      pb_debug       = TRUE)
 
 
-create_ge_plot_report(ps_gedir = "inst/extdata/compare-plot",
-                      ps_archdir = "inst/extdata/archive/compare-plot",
-                      ps_templ   = "inst/templates/compare_plots.Rmd.template")
+
--- bash/comp_plots_to_archive.sh
+++ bash/comp_plots_to_archive.sh
@@ -1,10 +1,116 @@
 #!/bin/bash
 
-RESTORE=/Users/pvr/Data/Projects/Github/pvrqualitasag/zwsroutinetools/bash/restore_from_archive.sh
-RESTORETRG=1904/compareBull
-ARCHDIR=/Volumes/data_archiv/zws/1904/health/mar/work/bv/zws/compareBull/
-WORKDIR=/Volumes/data_zws/health/mar/work/bv/zws/compareBull
 
+# ======================================== # ======================================= #
+# global constants                         #                                         #
+# ---------------------------------------- # --------------------------------------- #
+# prog paths                               #                                         #
+ECHO=/bin/echo                             # PATH to echo                            #
+DATE=/bin/date                             # PATH to date                            #
+BASENAME=/usr/bin/basename                 # PATH to basename function               #
+DIRNAME=/usr/bin/dirname                   # PATH to dirname function                #
+# ---------------------------------------- # --------------------------------------- #
+# directories                              #                                         #
+INSTALLDIR=`$DIRNAME ${BASH_SOURCE[0]}`    # installation dir of bashtools on host   #
+# ---------------------------------------- # --------------------------------------- #
+# files                                    #                                         #
+SCRIPT=`$BASENAME ${BASH_SOURCE[0]}`       # Set Script Name variable                #
+# ======================================== # ======================================= #
+
+
+
+### # ====================================================================== #
+### # functions
+usage () {
+  local l_MSG=$1
+  $ECHO "Usage Error: $l_MSG"
+  $ECHO "Usage: $SCRIPT -a <archive_dir> -t <restore_target> -w <work_dir> -r <restore_script>"
+  $ECHO "  where -a <archive_dir>     -- directory where plots are archived"
+  $ECHO "        -t <restore_target>  -- directory where archived files should be restored to"
+  $ECHO "        -w <work_dir>        -- working directory from ge where current plots are"
+  $ECHO ""
+  exit 1
+}
+
+### # produce a start message
+start_msg () {
+  $ECHO "Starting $SCRIPT at: "`$DATE +"%Y-%m-%d %H:%M:%S"`
+}
+
+### # produce an end message
+end_msg () {
+  $ECHO "End of $SCRIPT at: "`$DATE +"%Y-%m-%d %H:%M:%S"`
+}
+
+### # functions related to logging
+log_msg () {
+  local l_CALLER=$1
+  local l_MSG=$2
+  local l_RIGHTNOW=`$DATE +"%Y%m%d%H%M%S"`
+  $ECHO "[${l_RIGHTNOW} -- ${l_CALLER}] $l_MSG"
+}
+
+
+### # script starts here
+start_msg
+
+### # ====================================================================== #
+### # Use getopts for commandline argument parsing ###
+### # If an option should be followed by an argument, it should be followed by a ":".
+### # Notice there is no ":" after "h". The leading ":" suppresses error messages from
+### # getopts. This is required to get my unrecognized option code to work.
+RESTORE=/Users/pvr/Data/Projects/Github/pvrqualitasag/zwsroutinetools/bash/restore_from_archive.sh
+# RESTORETRG=1904/compareBull
+RESTORETRG=""
+# ARCHDIR=/Volumes/data_archiv/zws/1904/health/mar/work/bv/zws/compareBull/
+ARCHDIR=""
+# WORKDIR=/Volumes/data_zws/health/mar/work/bv/zws/compareBull
+WORKDIR=""
+
+while getopts ":a:t:w:h" FLAG; do
+  case $FLAG in
+    h)
+      usage "Help message for $SCRIPT"
+      ;;
+    a)
+      ARCHDIR=$OPTARG
+      ;;
+    t)
+      RESTORETRG=$OPTARG
+      ;;
+    w)
+      WORKDIR=$OPTARG
+      ;;
+    :)
+      usage "-$OPTARG requires an argument"
+      ;;
+    ?)
+      usage "Invalid command line argument -$OPTARG found"
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
+
+# Check whether required arguments have been defined
+if test "$ARCHDIR" == ""; then
+  usage "-a <archive_dir> not defined"
+fi
+
+if test "$RESTORETRG" == ""; then
+  usage "-t <restore_target> not defined"
+fi
+
+if test "$WORKDIR" == ""; then
+  usage "-w <work_dir> not defined"
+fi
+
+if test "$RESTORE" == ""; then
+  usage "-r <restore_script> not defined"
+fi
+
+
+### # change to work dir and start the comparision of the plots
 CURWD=`pwd`
 cd $WORKDIR
 
@@ -29,3 +135,7 @@ do
 done
 
 cd $CURWD
+
+
+### # script ends here
+end_msg
