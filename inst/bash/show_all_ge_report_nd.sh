79d15690b75f6459d06979adc7a88205b9f293f3
pvr <pvr@pvrs-MacBook-Pro.local>
2019-07-08 15:36:44 +0200

    Corrected names of reports in show script

--- inst/bash/show_all_ge_report_nd.sh
+++ inst/bash/show_all_ge_report_nd.sh
@@ -2,6 +2,6 @@
 
 for breed in bv rh
 do
-  qlmanage -p /Volumes/data_zws/fbk/work/${breed}/compare/ge_plot_report_fbk_compare_${breed}.pdf
+  qlmanage -p /Volumes/data_zws/nd/work/${breed}/compare/ge_plot_report_nd_compare_${breed}.pdf
   sleep 2
 done
