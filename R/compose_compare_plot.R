2c4dbe4d6b903701951e10d83c64d2a10bc14d18
pvr <pvr@pvrs-MacBook-Pro.local>
2019-07-30 14:46:39 +0200

    Replaced numeric ge- and gs-runlabels with strings

--- R/compose_compare_plot.R
+++ R/compose_compare_plot.R
@@ -853,24 +853,24 @@ create_ge_compare_plot_report_prod <- function(pn_cur_ge_label,
 #' A comparison plot report containing all generated plots of a GE side-by-side
 #' with the plots from the previous GE are constructed for VRDGGOZW
 #'
-#' @param pn_cur_ge_label label of current genetic evaluation (GE)
-#' @param pn_prev_ge_label label of previous GE
-#' @param pn_prevgsrun_label label of bi-weekly gs-runs before publication date of pn_prev_ge_label
+#' @param ps_cur_ge_label label of current genetic evaluation (GE)
+#' @param ps_prev_ge_label label of previous GE
+#' @param ps_prevgsrun_label label of bi-weekly gs-runs before publication date of ps_prev_ge_label
 #' @param ps_template template document for report
 #' @param pl_plot_opts list of options specifying input for plot report creator
 #' @param pb_debug flag whether debug output should be shown
 #' @examples
 #' \dontrun{
-#' create_ge_compare_plot_report_prod(pn_cur_ge_label  = 1908,
-#'                                  pn_prev_ge_label = 1904,
-#'                                  pn_prevgsrun_label = 0719,
+#' create_ge_compare_plot_report_prod(ps_cur_ge_label  = '1908',
+#'                                  ps_prev_ge_label = '1904',
+#'                                  ps_prevgsrun_label = '0719',
 #'                                  pb_debug = TRUE)
 #' }
 #'
 #' @export create_ge_compare_plot_report_vrdggozw
-create_ge_compare_plot_report_vrdggozw <- function(pn_cur_ge_label,
-                                               pn_prev_ge_label,
-                                               pn_prevgsrun_label,
+create_ge_compare_plot_report_vrdggozw <- function(ps_cur_ge_label,
+                                               ps_prev_ge_label,
+                                               ps_prevgsrun_label,
                                                ps_template  = system.file("templates", "compare_plots.Rmd.template", package = 'zwsroutinetools'),
                                                pl_plot_opts = NULL,
                                                pb_debug     = FALSE){
@@ -879,9 +879,9 @@ create_ge_compare_plot_report_vrdggozw <- function(pn_cur_ge_label,
     log_info(ps_caller = "create_ge_compare_plot_report_vrdggozw",
              ps_msg    = " * Start of function create_ge_compare_plot_report_vrdggozw")
     log_info(ps_caller = "create_ge_compare_plot_report_vrdggozw",
-             ps_msg    = paste0(" * Label of current GE: ", pn_cur_ge_label))
+             ps_msg    = paste0(" * Label of current GE: ", ps_cur_ge_label))
     log_info(ps_caller = "create_ge_compare_plot_report_vrdggozw",
-             ps_msg    = paste0(" * Label of previous GE: ", pn_prev_ge_label))
+             ps_msg    = paste0(" * Label of previous GE: ", ps_prev_ge_label))
   }
 
   # if no options are specified, we have to get the default options
@@ -912,9 +912,9 @@ create_ge_compare_plot_report_vrdggozw <- function(pn_cur_ge_label,
                  ps_msg    = paste0(" ** GE workdir: ", s_ge_dir, collapse = ""))
       # archive directory
       s_arch_dir <- file.path(l_plot_opts$arch_dir_stem,
-                              pn_prev_ge_label,
+                              ps_prev_ge_label,
                               "calcVRDGGOZW/work",
-                              paste0("result", pn_prevgsrun_label, collapse = ""),
+                              paste0("result", ps_prevgsrun_label, collapse = ""),
                               paste0(breed, "basis", collapse = ""),
                               paste0("comp", zwt, collapse = ""))
       if (pb_debug)
@@ -923,8 +923,8 @@ create_ge_compare_plot_report_vrdggozw <- function(pn_cur_ge_label,
 
       # Report text appears in all reports of this trait before the plots are drawn
       s_report_text  <- paste0('## Comparison Of Plots\nPlots compare estimates of VRDGGOZW for breed ', breed,
-                               ' between GE-run ', pn_prev_ge_label,
-                               ' on the left and the current GE-run ', pn_cur_ge_label,
+                               ' between GE-run ', ps_prev_ge_label,
+                               ' on the left and the current GE-run ', ps_cur_ge_label,
                                ' on the right.', collapse = "")
       if (pb_debug)
         log_info(ps_caller = "create_ge_compare_plot_report_vrdggozw",
@@ -932,7 +932,7 @@ create_ge_compare_plot_report_vrdggozw <- function(pn_cur_ge_label,
 
       # target directory
       l_arch_dir_split <- fs::path_split(s_arch_dir)
-      s_trg_dir <- file.path(pn_prev_ge_label, l_arch_dir_split[[1]][length(l_arch_dir_split[[1]])])
+      s_trg_dir <- file.path(ps_prev_ge_label, l_arch_dir_split[[1]][length(l_arch_dir_split[[1]])])
       if (pb_debug)
         log_info(ps_caller = "create_ge_compare_plot_report_vrdggozw",
                  ps_msg    = paste0(" ** Target directory for restored plots: ", s_trg_dir))
